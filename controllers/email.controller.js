facfb227b7a70a61e40024eb778545936dc333f5
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2021-07-07 15:02:20 +0800

    send email with mailtrap

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,6 +1,31 @@
+const nodemailer = require("nodemailer")
+require('dotenv').config()
+
 class EmailController{
     async email(req, res) {
-        return res.send('hello')
+
+        // TRANSPORTER
+        var transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: process.env.MAIL_PORT,
+            auth: {
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASSWORD
+            }
+        });
+
+        // SEND EMAIL
+        let info = await transporter.sendMail({
+            from: 'ryan@example.com', // sender address
+            to: 'customer@example.com', // receivers
+            subject: "Welcome", // Subject line
+            text: "Hello world, babe", // plain text body
+            html: "<b>Hello world</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        return res.send('success send')
     }
 }
 
